b20f335db8953b3507e13a701b4d634db20d6aee
ajex13 <ajex13@gmail.com>
2017-04-27 17:36:28 +0530

    add ui ,add post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-#after_create :send_welcome_note_to_user
+after_create :send_welcome_note_to_user
 
   def send_welcome_note_to_user
     NotificationMailer.signup_confirmation(self).deliver
