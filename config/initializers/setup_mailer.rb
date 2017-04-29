1e4cbc6a88eb5c970a8e0632c06d9353b9feea96
ajex13 <ajex13@gmail.com>
2017-04-29 15:46:30 +0530

    add favicon

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
    :domain => "gmail.com",
    :authentication => "plain",
    :enable_starttls_auto => true,
-   :user_name => "rortraining.welt@gmail.com",
-   :password => "secret123#@!",
+   :user_name => "ajinkya.ruby.dev@gmail.com",
+   :password => "Freedom@last",
    :openssl_verify_mode  => 'none'
 }
